bc8a55b96521aef58fcb12c3e596274720289f24
OlyaNegrych <olyanegrych@gmail.com>
2022-11-19 20:36:52 +0200

    added basename

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import {useEffect, lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
 import { useDispatch } from 'react-redux';
 import { PrivateRoute } from '../PrivateRoute';
@@ -8,7 +8,7 @@ import { refreshUser } from '../../redux/auth/operations';
 import { useAuth } from 'hooks';
 import NotFound from '../../pages/NotFound';
 
-const HomePage = lazy(() => import('../../pages/HomePage'));
+// const HomePage = lazy(() => import('../../pages/HomePage'));
 const ContactsPage = lazy(() => import('../../pages/Contacts/ContactsPage'));
 const RegisterPage = lazy(() => import('../../pages/Register/RegisterPage'));
 const LoginPage = lazy(() => import('../../pages/Login/LoginPage'));
@@ -24,36 +24,34 @@ const App = () => {
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<RegisterPage />}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<LoginPage />}
-              />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<HomePage />} /> */}
+        <Route index element={<Navigate to="/login" replace />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
 
